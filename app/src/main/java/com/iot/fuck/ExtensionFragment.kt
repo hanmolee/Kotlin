219a2ad66414b78a0083649c91e4783e8fa9673f
hanmolee <dlgksah@gmail.com>
2017-05-31 15:43:16 +0900

    Initial commit

---
+++ app/src/main/java/com/iot/fuck/ExtensionFragment.kt
@@ -0,0 +1,21 @@
+package com.iot.fuck
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_extension_fragment.*
+
+class ExtensionFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.activity_extension_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {//이때부터 extension을 통해 뷰 객체에 접근가능
+        button.setOnClickListener {
+            textView.text = "hello Fragment, ${editTect.text.toString()}"
+        }
+    }
+}
