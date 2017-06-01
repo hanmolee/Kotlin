c089ec2275af49841a05ddae1d7b4d21cf0af4ae
hanmolee <dlgksah@gmail.com>
2017-06-01 09:21:58 +0900

    코틀린으로
    리싸이클뷰 구현하는 연습
    
    전체적내용은 안드로이드 틀을 벗어나지 않는다
    
    다만 코틀린의 문법을 모르기 때문에 자바처럼 응용하기는
    아직 시간이 더 걸릴것 같다
    
    2.
    Intent로 Ativity 전환하는 연습을 하였다
    다시 내것으로 만들어 다시 Commit 하겠음
    
    3.
    Intent를 정리해서 자바처럼 인텐트를 넘길 수 있도록 하였다.
    
    4.
    with함수에 대해 주석을 달아놈

--- app/src/main/java/com/iot/fuck/Kotlin_RecyclerView.kt
+++ app/src/main/java/com/iot/fuck/Kotlin_RecyclerView.kt
@@ -11,10 +11,11 @@ class Kotlin_RecyclerView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin__recycler_view)
 
-        with(recycler_view) {//with 란??  포함하다??
+        with(recycler_view) {//with 란 하나의 인스턴스에 여러 메소드를 쓰기위한 함수
             layoutManager = LinearLayoutManager(this@Kotlin_RecyclerView)
             adapter = FeedAdapter()
-
+            //with의 예시 = Java의 a.sum, a.max, a.min을
+            //              with(a){ sum, max, min } 이런식으로 쓸 수 있다
         }
     }
 }
